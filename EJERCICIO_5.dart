82303671dad61afe90cb525323b635932b45a4aa
Amidwar <amidwar05@gmail.com>
2026-02-18 20:13:19 -0500

    Finalizando ejercicio 5

---
+++ EJERCICIO_5.dart
@@ -0,0 +1,20 @@
+//EJERCICIO 5: Conteo de frecuencia de palabras
+
+import 'dart:io';
+
+void main(){
+  stdout.write("Ingrese una frase: ");
+  String frase = stdin.readLineSync()!.toLowerCase();
+
+  List<String> palabras = frase.split(" ");
+  Map<String, int> frecuencia = {};
+
+  for(String palabra in palabras){
+    frecuencia[palabra]=(frecuencia[palabra]??0)+1;
+  }
+
+  print("\nFrecuencia de palabras: ");
+  frecuencia.forEach((palabra, conteo){
+    print("$palabra: $conteo");
+  });
+}
